d885741a7de4b054c2e3effeafca2e19c0520f51
andrewdunetz01 <andrewdunetz01@vt.edu>
2021-02-26 17:25:20 -0500

    first commit

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyABAZ_5bTQiOZS_wxvRp-7Zg6LTsGvmzco",
+  authDomain: "crwn-db-82593.firebaseapp.com",
+  projectId: "crwn-db-82593",
+  storageBucket: "crwn-db-82593.appspot.com",
+  messagingSenderId: "273723117644",
+  appId: "1:273723117644:web:bae3be52f2a70e5fe664e9",
+  measurementId: "G-SWGVXKNZ75"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const addCollectionAndDocuments = (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef)
+}
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider).catch(()=>{});
+
+export default firebase;
